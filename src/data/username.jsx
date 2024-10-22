8c973127d62a778108645973e33f4c44a63ed002
nattapong486 <nattapong.raa@spumail.net>
2024-10-22 22:18:03 +0700

    delete commit

--- src/data/username.jsx
+++
@@ -1,9 +0,0 @@
-const uesrnames = [
-  { user: "user", pass: "pass", role: "admin", token: "user" },
-];
-
-export function checkUsername(user, pass) {
-  const userInfo = uesrnames.find((u) => u.user === user && u.pass === pass);
-
-  return userInfo ? { token: userInfo.token, role: userInfo.role } : null;
-}
